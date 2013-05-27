bb3c83f6e3995c5980fe55c1c6485ae0989c7c64
gtss <yourname1111@gmail.com>
2013-05-27 20:57:55 +0800

    update project info

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject coreweb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library designed to show meta of clojure.core"
+  :url "https://github.com/gtss/coreweb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.incubator "0.1.0"]
                  [ring "1.1.8"]]
   :repositories {"local" "/Users/yourname/.m2"}
   :main coreweb.main)
